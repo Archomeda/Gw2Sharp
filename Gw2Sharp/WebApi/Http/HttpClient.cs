a8cd997583e632276088119a38a93674661d1ddf
Archomeda <archomeda@hotmail.com>
2019-04-12 00:17:45 +0200

    Expand HttpClient a bit to support external instances

--- Gw2Sharp/WebApi/Http/HttpClient.cs
+++ Gw2Sharp/WebApi/Http/HttpClient.cs
@@ -12,7 +12,34 @@ namespace Gw2Sharp.WebApi.Http
     /// </summary>
     public class HttpClient : IHttpClient
     {
-        private static readonly SysHttpClient SysHttpClient = new SysHttpClient();
+        private static readonly SysHttpClient sysHttpClient = new SysHttpClient();
+        private readonly Func<SysHttpClient> getSysHttpClient = () => sysHttpClient;
+        private readonly bool shouldDisposeHttpClient = false;
+
+        /// <summary>
+        /// Creates a new <see cref="HttpClient"/> with a built-in <see cref="SysHttpClient"/> instance.
+        /// Not recommended if you are running Gw2Sharp for a longer period of time, because of possible DNS changes.
+        /// Use <see cref="HttpClient(Func{SysHttpClient})"/> instead that refreshes the HttpClient once in a while (e.g. by using Microsoft.Extensions.Http).
+        /// </summary>
+        public HttpClient() { }
+
+        /// <summary>
+        /// Creates a new <see cref="HttpClient"/> with a provided <see cref="SysHttpClient"/> instance.
+        /// Not recommended if you are running Gw2Sharp for a longer period of time, because of possible DNS changes.
+        /// Use <see cref="HttpClient(Func{SysHttpClient})"/> instead that refreshes the HttpClient once in a while (e.g. by using Microsoft.Extensions.Http).
+        /// </summary>
+        public HttpClient(SysHttpClient httpClient) : this(() => httpClient) { }
+
+        /// <summary>
+        /// Creates a new <see cref="HttpClient"/> with a provided function that returns a <see cref="SysHttpClient"/> instance.
+        /// After every request, the <see cref="SysHttpClient"/> is automatically disposed.
+        /// Be sure to prevent over-disposing by using e.g. Microsoft.Extensions.Http.
+        /// </summary>
+        public HttpClient(Func<SysHttpClient> getHttpClientFunc)
+        {
+            this.getSysHttpClient = getHttpClientFunc;
+            this.shouldDisposeHttpClient = true;
+        }
 
         /// <inheritdoc />
         public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(30);
@@ -27,11 +54,16 @@ namespace Gw2Sharp.WebApi.Http
                 message.Headers.AddRange(request.RequestHeaders);
 
                 Task<HttpResponseMessage>? task = null;
+                SysHttpClient? httpClient = null;
                 HttpResponseMessage responseMessage;
                 HttpResponse response;
                 try
                 {
-                    task = SysHttpClient.SendAsync(message, linkedCancellation.Token);
+                    httpClient = this.getSysHttpClient();
+                    if (httpClient == null)
+                        throw new InvalidOperationException("HttpClient is null");
+
+                    task = httpClient.SendAsync(message, linkedCancellation.Token);
                     responseMessage = await task.ConfigureAwait(false);
 
                     response = new HttpResponse(
@@ -40,7 +72,6 @@ namespace Gw2Sharp.WebApi.Http
                         request.RequestHeaders.ToDictionary(kvp => kvp.Key, kvp => kvp.Value),
                         responseMessage.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First())
                     );
-
                 }
                 catch (Exception ex)
                 {
@@ -49,6 +80,11 @@ namespace Gw2Sharp.WebApi.Http
                     else if (task.IsCanceled)
                         throw new RequestCanceledException(request);
                     throw new RequestException(request, $"Request error: {task?.Exception?.Message}", ex);
+                }
+                finally
+                {
+                    if (this.shouldDisposeHttpClient)
+                        httpClient?.Dispose();
                 }
 
                 if (!responseMessage.IsSuccessStatusCode)
