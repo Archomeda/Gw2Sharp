f5ace81aafc6305b28abc4cbd4a4a1f7e3f1afc9
Archomeda <archomeda@hotmail.com>
2019-06-15 02:58:47 +0200

    Add missing endpoint schema version to /v2/account/dailycrafting

--- Gw2Sharp/WebApi/V2/Clients/Account/AccountDailyCraftingClient.cs
+++ Gw2Sharp/WebApi/V2/Clients/Account/AccountDailyCraftingClient.cs
@@ -6,6 +6,7 @@ namespace Gw2Sharp.WebApi.V2.Clients
     /// A client of the Guild Wars 2 API v2 account dailycrafting endpoint.
     /// </summary>
     [EndpointPath("account/dailycrafting")]
+    [EndpointSchemaVersion("2019-02-21T00:00:00.000Z")]
     public class AccountDailyCraftingClient : BaseEndpointBlobClient<IApiV2ObjectList<string>>, IAccountDailyCraftingClient
     {
         /// <summary>
