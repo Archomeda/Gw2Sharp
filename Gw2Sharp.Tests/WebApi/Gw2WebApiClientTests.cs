f6f9e49906e4b7771446ff84e4a9f3cdf61c18c3
Archomeda <archomeda@hotmail.com>
2019-06-19 21:59:08 +0200

    Actually refactor the Connection property out of non endpoint clients

--- Gw2Sharp.Tests/WebApi/Gw2WebApiClientTests.cs
+++ Gw2Sharp.Tests/WebApi/Gw2WebApiClientTests.cs
@@ -1,4 +1,4 @@
-﻿using Gw2Sharp.WebApi;
+using Gw2Sharp.WebApi;
 using Gw2Sharp.WebApi.V2;
 using NSubstitute;
 using Xunit;
@@ -15,10 +15,8 @@ namespace Gw2Sharp.Tests.WebApi
             var client1 = new Gw2WebApiClient();
             var client2 = new Gw2WebApiClient(connection);
 
-            Assert.IsType<Connection>(client1.Connection);
             Assert.IsType<Gw2WebApiV2Client>(client1.V2);
 
-            Assert.Same(connection, client2.Connection);
             Assert.IsType<Gw2WebApiV2Client>(client2.V2);
         }
     }
--- Gw2Sharp/WebApi/Gw2WebApiClient.cs
+++ Gw2Sharp/WebApi/Gw2WebApiClient.cs
@@ -6,7 +6,7 @@ namespace Gw2Sharp.WebApi
     /// <summary>
     /// A client for the Guild Wars 2 web API.
     /// </summary>
-    public class Gw2WebApiClient : IGw2WebApiClient, IWebApiClientInternal
+    public class Gw2WebApiClient : IGw2WebApiClient
     {
         /// <summary>
         /// The base URL for making Guild Wars 2 API requests.
@@ -24,19 +24,14 @@ namespace Gw2Sharp.WebApi
         /// Creates a new <see cref="Gw2WebApiClient"/>.
         /// </summary>
         /// <param name="connection">The connection used to make requests, see <see cref="IConnection"/>.</param>
+        /// <exception cref="NullReferenceException"><paramref name="connection"/> is <c>null</c>.</exception>
         public Gw2WebApiClient(IConnection connection)
         {
-            this.Connection = connection;
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+
             this.v2 = new Gw2WebApiV2Client(connection);
         }
-
-        /// <inheritdoc />
-        IConnection IWebApiClientInternal.Connection => this.Connection;
-
-        /// <summary>
-        /// Provides the client connection to make web requests.
-        /// </summary>
-        internal IConnection Connection { get; }
 
         /// <inheritdoc />
         public virtual IGw2WebApiV2Client V2 => this.v2;
