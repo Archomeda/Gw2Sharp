c784f22d4a538c84c4b1188bfd0a3f37bf79ccef
Dade Lamkins <dade@lamkins.org>
2020-01-22 21:45:26 -0500

    Added null check short circuit to GuidConverter's ReadJson to prevent ArgumentNullException when requesting character information of a character not actively representing a guild.  Fixes #27.

--- Gw2Sharp/Json/Converters/GuidConverter.cs
+++ Gw2Sharp/Json/Converters/GuidConverter.cs
@@ -13,9 +13,13 @@ namespace Gw2Sharp.Json.Converters
         public override bool CanWrite => false;
 
         /// <inheritdoc />
-        public override Guid ReadJson(JsonReader reader, Type objectType, Guid existingValue, bool hasExistingValue, JsonSerializer serializer) =>
-            new Guid(serializer.Deserialize<string>(reader));
+        public override Guid ReadJson(JsonReader reader, Type objectType, Guid existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
 
+            return new Guid(serializer.Deserialize<string>(reader));
+        }
+            
         /// <inheritdoc />
         public override void WriteJson(JsonWriter writer, Guid value, JsonSerializer serializer) =>
             throw new NotImplementedException("TODO: This should generally not be used since we only deserialize stuff from the API, and not serialize to it. Might add support later.");
