8db102a8e7bf79de9891842d03f2bd1ad130828c
Archomeda <archomeda@hotmail.com>
2020-01-25 13:17:55 +0100

    Add additional test to check ArchiveCache for object storage
    (cherry picked from commit 2cb7136ff6a19490730caa2862e1e55b9131a179)

--- Gw2Sharp.Tests/WebApi/Caching/ArchiveCacheMethodTests.cs
+++ Gw2Sharp.Tests/WebApi/Caching/ArchiveCacheMethodTests.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Text;
 using System.Threading.Tasks;
 using Gw2Sharp.Tests.Helpers;
 using Gw2Sharp.WebApi.Caching;
@@ -21,7 +23,7 @@ namespace Gw2Sharp.Tests.WebApi.Caching
 
 
         [Fact]
-        public async Task StoresCacheIntoArchiveTest()
+        public async Task StoresRawCacheIntoArchiveTest()
         {
             string category = "testdata";
             string id = "bytearray.dat";
@@ -42,6 +44,32 @@ namespace Gw2Sharp.Tests.WebApi.Caching
             await entryStream.CopyToAsync(memoryStream);
             byte[] actual = memoryStream.ToArray();
             Assert.Equal(data, actual);
+        }
+
+        [Fact]
+        public async Task StoresArrayCacheIntoArchiveTest()
+        {
+            string category = "testdata";
+            string id = "stringarray.dat";
+            var expiryTime = DateTimeOffset.UtcNow.AddMinutes(1);
+
+            string[] data = new[] { "Hello", "World!" };
+            await this.cacheMethod.SetAsync(category, id, data, expiryTime);
+            var actualCache = await this.cacheMethod.TryGetAsync<IEnumerable<string>>(category, id);
+
+            Assert.Equal(data, actualCache?.Item);
+            this.cacheMethod.Dispose();
+
+            using var stream = File.OpenRead(ARCHIVE_FILENAME);
+            var archive = new ZipArchive(stream);
+            var entry = archive.GetEntry($"{category}/{id}");
+            using var entryStream = entry.Open();
+            using var memoryStream = new MemoryStream();
+            await entryStream.CopyToAsync(memoryStream);
+
+            string expected = $"[\"{data[0]}\",\"{data[1]}\"]";
+            string actual = Encoding.UTF8.GetString(memoryStream.ToArray());
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
