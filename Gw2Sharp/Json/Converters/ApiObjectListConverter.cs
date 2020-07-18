d5795a4ab283e7c58cd37ddf8dc1f4e67c4b1cf5
Archomeda <archomeda@hotmail.com>
2020-07-18 21:11:15 +0200

    Make some more forgotten changes to the custom converter

--- Gw2Sharp/Json/Converters/ApiObjectListConverter.cs
+++ Gw2Sharp/Json/Converters/ApiObjectListConverter.cs
@@ -23,13 +23,13 @@ namespace Gw2Sharp.Json.Converters
         {
             var innerType = typeToConvert.GetGenericArguments()[0];
             var type = typeof(ApiObjectListConverterInner<>).MakeGenericType(innerType);
-            return (JsonConverter?)Activator.CreateInstance(type, BindingFlags.Instance | BindingFlags.Public, null, new[] { options }, null);
+            return (JsonConverter?)Activator.CreateInstance(type, BindingFlags.Instance | BindingFlags.Public, null, null, null);
         }
 
         private sealed class ApiObjectListConverterInner<T> : JsonConverter<ApiV2BaseObjectList<T>>
         {
             public override ApiV2BaseObjectList<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
-                JsonSerializer.Deserialize<ApiV2BaseObjectList<T>>(ref reader, options)!;
+                JsonSerializer.Deserialize<ApiV2BaseObjectList<T>>(ref reader, options);
 
             public override void Write(Utf8JsonWriter writer, ApiV2BaseObjectList<T> value, JsonSerializerOptions options) =>
                 throw new NotImplementedException("TODO: This should generally not be used since we only deserialize stuff from the API, and not serialize to it. Might add support later.");
